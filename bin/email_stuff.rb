1cf5dda98cdd18c253bfd46f05c5aecf2d4ad4af
Georgios Gousios <gousiosg@gmail.com>
2014-02-18 21:20:58 +0100

    Fix email sending, inserting into MySQL

--- bin/email_stuff.rb
+++ bin/email_stuff.rb
@@ -1,12 +1,17 @@
+require 'net/smtp'
+
 module EmailStuff
 
   REQ_SUCCEEDED = <<-EMAIL
+Subject: GHTorrent request
+Date: %s
+
 Dear %s,
 
 Your request succedeed. Your request id is: %s.
 You can view your request's status here:
 
-  %s
+  http://ghtorrent.org/lean%s
 
 When your request has finished processing, you will receive a new email.
 
@@ -19,19 +24,19 @@ Thank you for using GHTorrent!
 
 
   def send_req_succeed(email, name, id, url)
-    text = sprintf(REQ_SUCCEEDED, name, id, url)
+    text = sprintf(REQ_SUCCEEDED, Time.now, name, id, url)
     send_email(email, text)
   end
 
   def send_email(to, text)
-    #Net::SMTP.start('localhost', 25, 'GHTorrent<ghtorrent.org>') do |smtp|
-    #  begin
-    #    smtp.send_message(text, 'noreply@ghtorrent.org', to)
-    #  rescue Exception => e
-    #    logger.error "Failed to send email to #{to}: #{e.message}"
-    #    logger.error e.backtrace.join("\n")
-    #  end
-    #end
+    Net::SMTP.start('localhost', 25, 'ghtorrent.org') do |smtp|
+      begin
+        smtp.send_message(text, 'GHTorrent Service<noreply@ghtorrent.org>', to)
+      rescue Exception => e
+        logger.error "Failed to send email to #{to}: #{e.message}"
+        logger.error e.backtrace.join("\n")
+      end
+    end
   end
 
 end
