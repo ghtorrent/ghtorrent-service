ed89a788e7a7435bce0571518b60e36129614f5e
Georgios Gousios <gousiosg@gmail.com>
2014-07-03 15:51:31 +0200

    Remove dead code

--- python/config.py
+++
@@ -1,8 +0,0 @@
-CSRF_ENABLED = True
-SECRET_KEY = '1340e3bb4dc844b1abf246ef14afb6e3'
-
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
